4ad24f6f0a280eb4c0f05822498cb95a643829a0
harshitha811 <67686388+harshitha811@users.noreply.github.com>
2020-12-21 22:03:37 +0530

    Add files via upload

---
+++ LAB4/lab4.c
@@ -0,0 +1,73 @@
+/*WAP to simulate the working of a circular queue of integers using an array. Provide the
+following operations.
+a) Insert b) Delete c) Display
+The program should print appropriate messages for queue empty and queue overflow
+conditions*/
+#include<stdio.h>
+#include<conio.h>
+#include<process.h>
+#define QUE_SIZE 3
+int item,front=0,rear=-1,q[QUE_SIZE],count=0;
+void insertrear()
+{
+if(count==QUE_SIZE)
+{
+printf("queue overflow\n");
+return;
+}
+rear=(rear+1)%QUE_SIZE;
+q[rear]=item;
+count++;
+}
+int deletefront()
+{
+if(count==0) return -1;
+item=q[front];
+front=(front+1)%QUE_SIZE;
+count=count-1;
+return item;
+}
+void displayQ()
+{
+int i,f;
+if(count==0)
+{
+printf("queue is empty\n");
+return;
+}
+f=front;
+printf("Contents of queue \n");
+for(i=1;i<=count;i++)
+{
+printf("%d\n",q[f]);
+f=(f+1)%QUE_SIZE;
+}
+}
+int main()
+{
+ int choice;
+ for(;;)
+ {
+ printf("\n1:insertrear\n2:deletefront\n3:display\n4:exit\n");
+ printf("enter the choice\n");
+ scanf("%d",&choice);
+
+ switch(choice)
+ {
+ case 1:printf("enter the item to be inserted\n");
+	scanf("%d",&item);
+	insertrear();
+	break;
+ case 2:item=deletefront();
+	if(item==-1)
+	printf("queue is empty\n");
+	else
+	printf("item deleted =%d\n",item);
+	break;
+ case 3:displayQ();
+	break;
+ default:exit(0);
+}
+}
+return 0;
+}
